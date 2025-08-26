1944f793921b2dabad69af79f5d0125f25f14e62
Giftson Samuel <giftson.samuel0705@gmail.com>
2025-08-26 10:55:09 +0530

    all spi methodes (get, post, put, delete)

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from fastapi import FastAPI
+# from pydantic import BaseModel,EmailStr,conint,constr,field_validators
+from fastapi import Form, UploadFile, File 
 
 app = FastAPI()
 
+# class User(BaseModel):
+#     name: str
+#     age: int 
+#     email:str
+
 @app.get("/")
 def read_root():
-    return {"msg": "FastAPI is running locally!"}
+    return {"message": "Hello, World!"}
+
+@app.get("/users/{user_id}")
+def road_user(user_id: int):
+   return {"user_id": user_id}
+
+@app.get("/items/")
+def read_items(skip: int = 0, limit: int = 10):
+   return {"skip": skip, "limit": limit}   
+
+
+@app.post("/items/")
+def create_item(name: str, price: float):
+    return {"item_name": name, "price": price}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, name: str, price = float):
+    return {"item_id": item_id, "name": name, "price": price}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):  
+    return {"message": f"Item {item_id} deleted"}
+
+ 
+
